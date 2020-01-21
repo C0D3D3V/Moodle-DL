fda0729765f78b6763eb81c58d807882061428b4
c0d3d3v <live.for.program@gmail.com>
2020-01-21 14:01:03 +0100

    Refactoring Downloader

--- download_service/downlaoder.py
+++ download_service/downlaoder.py
@@ -1,14 +1,25 @@
 import threading
+
 from queue import Queue
+
 from utils.state_recorder import StateRecorder
 
 
 class Downloader(threading.Thread):
+    """
+    Downlaoder processes the queue and puts an
+    url target back into the queue if an error occurs.
+    """
+
     def __init__(self, queue: Queue, report: [],
                  state_recorder: StateRecorder,
                  thread_id: int,
                  lock: threading.Lock):
+        """
+        Initiates a downloader thread.
+        """
         threading.Thread.__init__(self)
+
         self.queue = queue
         self.report = report
         self.thread_id = thread_id
@@ -18,19 +29,29 @@ class Downloader(threading.Thread):
     def run(self):
         while self.queue.empty() is False:
             try:
+                # raise condition
                 url_target = self.queue.get(False)
             except Queue.Empty:
                 break
 
             response = url_target.download(self.thread_id)
 
-            # information is still saved in url_target
+            # All information is still saved in url_target
+
+            # If a download fails but the maximum number of
+            # attempts is not reached, the url target would
+            # be returned to the queue
             if (response is False and
                     url_target.url_tried < url_target.url_tries):
                 self.queue.put(url_target)
+
+            # If a download fails but the maximum number of
+            # attempts is exhausted, add it to the error report.
             elif (response is False and
                   url_target.url_tried == url_target.url_tries):
                 self.report['failure'].append(url_target)
+
+            # If a download was successful, store it in the database.
             elif (response):
                 self.lock.acquire()
                 self.state_recorder.save_file(
