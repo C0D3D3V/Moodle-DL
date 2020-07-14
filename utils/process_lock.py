91228ab7aeabcaf5a967e7552d6cb36265c74d04
Daniel Vogt <daniel@vogt.ml>
2020-07-14 14:28:36 +0200

    Fix Process lock delete

--- utils/process_lock.py
+++ utils/process_lock.py
@@ -1,34 +1,31 @@
-from pathlib import Path
-
 """
-A very simple lock mechanism to prevent multiple downloaders
-being started for the same Moodle.
+A very simple lock mechanism to prevent multiple downloaders being started for the same Moodle.
 
 The functions are not resistant to high frequency calls.
 Raise conditions will occur!
 """
 
+from pathlib import Path
+
 
 class LockError(Exception):
-    """An Exception which gets thrown if a Downloader is already running"""
+    """An Exception which gets thrown if a Downloader is already running."""
+
     pass
 
 
 def lock(dir_path: str):
-    """
-    Tests if a lock is already set in a directory, if not it creates the lock
-    """
+    """Test if a lock is already set in a directory, if not it creates the lock."""
     path = Path(dir_path) / "running.lock"
-    if(Path(path).exists()):
-        raise LockError(
-            "A downloader is already running." +
-            " Delete {} if you think this is wrong.".format(str(path)))
+    if Path(path).exists():
+        raise LockError("A downloader is already running." + " Delete {} if you think this is wrong.".format(str(path)))
     Path(path).touch()
 
 
 def unlock(dir_path: str):
-    """
-    Removes a lock in a directory
-    """
+    """Remove a lock in a directory."""
     path = Path(dir_path) / "running.lock"
-    Path(path).unlink(missing_ok=True)
+    try:
+        Path(path).unlink(missing_ok=True)
+    except Exception:
+        pass
