57c774152a58bc0fafe94473938cd66966b29b81
Jonas T. Schmid <jonas.till.schmid@gmail.com>
2022-02-18 15:09:33 +0100

    Implemented commit_all_changes and add_file_to_git and removed steady state

--- moodle_dl/git_service/git_service.py
+++ moodle_dl/git_service/git_service.py
@@ -1,21 +1,25 @@
 import git
 
 class git_service:
-    repoindex = []
-    courses = []
-    def __init__(self, courses):
+    def __init__(self, courses, storage_path: str):
+        self.storage_path = storage_path
         for course in courses:
             try:
                 repo = git.Repo(course.fullname)
             except git.exc.InvalidGitRepositoryError:
                 self.init_git(self, course)
                 repo = git.Repo(course.fullname)
-            self.repoindex.append(repo)
-            self.courses.append(course)
+
 
     def init_git(self, course):
         git.Repo.init(course.fullname, mkdir=True)
 
 
-    def add_file_to_git(course, path, file):
+    def add_file_to_git(self, course, path, file):
+        repo = git.Repo(course.fullname)
+        repo.index.add(path)
 
+    def commit_all_changes(self, courses):
+        for course in courses:
+            repo = git.Repo(course.fullname)
+            repo.index.commit()
