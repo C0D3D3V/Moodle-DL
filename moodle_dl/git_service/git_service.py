729e0cd116c1f73e98e5d4c615967ba281c26e33
Jonas T. Schmid <jonas.till.schmid@gmail.com>
2022-02-21 18:10:14 +0100

    I added some functions, simplified git calls

--- moodle_dl/git_service/git_service.py
+++ moodle_dl/git_service/git_service.py
@@ -1,8 +1,21 @@
+import os
+
 import git
 
 import datetime
+
+
 class git_service:
     def __init__(self, courses, storage_path: str):
+        self.storage_path = storage_path
+        for course in courses:
+            try:
+                repo = git.Repo(course.fullname)
+            except git.exc.InvalidGitRepositoryError:
+                self.init_git(course)
+                repo = git.Repo(course.fullname)
+
+    def startgit(self, courses, storage_path):
         self.storage_path = storage_path
         for course in courses:
             try:
@@ -11,13 +24,18 @@ class git_service:
                 self.init_git(self, course)
                 repo = git.Repo(course.fullname)
 
-
     def init_git(self, course):
         git.Repo.init(course.fullname, mkdir=True)
-
-
-    def add_file_to_git(self, course, path, file):
-        repo = git.Repo(course.fullname)
+    def add_all_files_to_git(self, courses):
+        for course in courses:
+            repo = git.Repo(course.fullname)
+            repo.index.add(repo.untracked_files)
+    def add_file_to_git(self, course, path, file=None):
+        try:
+            repo = git.Repo(course.fullname)
+        except git.exc.InvalidGitRepositoryError:
+            self.init_git(course)
+            repo = git.Repo(course.fullname)
         repo.index.add(path)
 
     def commit_all_changes(self, courses):
