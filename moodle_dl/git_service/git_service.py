8676fd50c29ffe9dcc7ea13eb7422d336f963605
Jonas T. Schmid <jonas.till.schmid@gmail.com>
2022-02-22 10:44:15 +0100

    Now using function get_folder(course) in DownloadService
    As discussed in PR #137, the User can and maybe will change the folder where the files are stored.

--- moodle_dl/git_service/git_service.py
+++ moodle_dl/git_service/git_service.py
@@ -4,16 +4,19 @@ import git
 
 import datetime
 
+import moodle_dl.download_service.download_service as dl
 
 class git_service:
     def __init__(self, courses, storage_path: str):
         self.storage_path = storage_path
+
         for course in courses:
+            folder = dl.DownloadService.get_folder(course)
             try:
-                repo = git.Repo(course.fullname)
+                repo = git.Repo(folder)
             except git.exc.InvalidGitRepositoryError:
                 self.init_git(course)
-                repo = git.Repo(course.fullname)
+                repo = git.Repo(folder)
 
     def startgit(self, courses, storage_path):
         self.storage_path = storage_path
@@ -25,20 +28,23 @@ class git_service:
                 repo = git.Repo(course.fullname)
 
     def init_git(self, course):
-        git.Repo.init(course.fullname, mkdir=True)
+        folder = dl.DownloadService.get_folder(course)
+        git.Repo.init(folder, mkdir=True)
     def add_all_files_to_git(self, courses):
         for course in courses:
-            repo = git.Repo(course.fullname)
+            folder = dl.DownloadService.get_folder(course)
+            repo = git.Repo(folder)
             repo.index.add(repo.untracked_files)
     def add_file_to_git(self, course, path, file=None):
+        folder = dl.DownloadService.get_folder(course)
         try:
-            repo = git.Repo(course.fullname)
+            repo = git.Repo(folder)
         except git.exc.InvalidGitRepositoryError:
-            self.init_git(course)
-            repo = git.Repo(course.fullname)
+            self.init_git(folder)
+            repo = git.Repo(folder)
         repo.index.add(path)
 
     def commit_all_changes(self, courses):
         for course in courses:
-            repo = git.Repo(course.fullname)
+            repo = git.Repo(dl.DownloadService.get_folder(course))
             repo.index.commit(datetime.datetime.now().strftime("%c"))
