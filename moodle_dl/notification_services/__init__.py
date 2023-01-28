07b8d960dbdf12187161c4bf9cbe30560d8234d5
c0d3d3v <c0d3d3v@mag-keinen-spam.de>
2023-01-28 20:42:09 +0100

    Add flake8 github action

--- moodle_dl/notification_services/__init__.py
+++ moodle_dl/notification_services/__init__.py
@@ -5,9 +5,11 @@ from moodle_dl.notification_services.notification_service import NotificationSer
 
 from moodle_dl.notification_services.console.console_service import ConsoleService
 
-from moodle_dl.notification_services.mail.mail_service import MailService  # noqa: F401
-from moodle_dl.notification_services.telegram.telegram_service import TelegramService  # noqa: F401
-from moodle_dl.notification_services.xmpp.xmpp_service import XmppService  # noqa: F401
+from moodle_dl.notification_services.mail.mail_service import MailService
+from moodle_dl.notification_services.telegram.telegram_service import TelegramService
+from moodle_dl.notification_services.xmpp.xmpp_service import XmppService
+
+__all__ = ['ConsoleService', 'MailService', 'TelegramService', 'XmppService']
 
 REMOTE_SERVICES = [Class for name, Class in globals().items() if name.endswith('Service') and name != 'ConsoleService']
 ALL_SERVICES = [ConsoleService] + REMOTE_SERVICES
