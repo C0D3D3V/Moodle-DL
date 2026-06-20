73ad047caa7b93228047fa23610c1c7226dcde4b
chmanny <chmanny>
2026-06-20 05:02:39 +0200

    Refactor kalvidres LTI extractor to improve URL extraction and handling of launch forms

--- moodle_dl/downloader/extractors/kalvidres_lti.py
+++ moodle_dl/downloader/extractors/kalvidres_lti.py
@@ -31,33 +31,66 @@ class KalvidresLtiIE(InfoExtractor):
 
         # Get launch parameters
         launch_webpage = self._download_webpage(launch_url, video_id, 'Downloading kalvidres launch webpage')
-        launch_inputs = self._form_hidden_inputs(self._LAUNCH_FORM, launch_webpage)
+
         launch_form_str = self._search_regex(
-            fr'(?P<form><form[^>]+?id=(["\']){self._LAUNCH_FORM}\2[^>]*>)', launch_webpage, 'login form', group='form'
+            r'(?P<form><form[^>]*>)', launch_webpage, 'launch form', group='form', default=None
         )
 
-        action_url = extract_attributes(launch_form_str).get('action')
+        if launch_form_str:
+            action_url = extract_attributes(launch_form_str).get('action') or launch_url
+            launch_inputs = self._hidden_inputs(launch_webpage)
 
-        # Launch kalvidres video app
-        submit_page, dummy = self._download_webpage_handle(
-            action_url, video_id, 'Launch kalvidres app', data=urlencode_postdata(launch_inputs)
-        )
+            # Launch kalvidres video app
+            submit_page, dummy = self._download_webpage_handle(
+                action_url, video_id, 'Launch kalvidres app', data=urlencode_postdata(launch_inputs)
+            )
+        else:
+            submit_page = launch_webpage
 
-        mobj = re.search(r'window.location.href = \'(?P<url>[^\']+)\'', submit_page)
-        if not mobj:
-            raise ExtractorError('Unable to extract kalvidres redirect url')
-
-        # Follow kalvidres video app redirect
-        redirect_page, dummy = self._download_webpage_handle(
-            html.unescape(mobj.group('url')), video_id, 'Follow kalvidres redirect'
-        )
+        # Follow kalvidres video app redirect if present
+        mobj = re.search(r'window(?:(?:\.top|\.parent))?\.location\.(?:href|replace)\s*(?:=|)\s*\(?[\'"](?P<url>[^\'"]+)[\'"]\)?', submit_page)
+        if mobj:
+            redirect_page, dummy = self._download_webpage_handle(
+                html.unescape(mobj.group('url')), video_id, 'Follow kalvidres redirect'
+            )
+        else:
+            redirect_page = submit_page
 
         kultura_url = KalturaIE._extract_url(redirect_page)  # pylint: disable=protected-access
-        if not kultura_url:
-            raise ExtractorError('Unable to extract kaltura url')
+        partner_id = entry_id = None
+
+        if kultura_url:
+            m = re.match(r'^kaltura:(\d+):([^:]+)', kultura_url)
+            if m:
+                partner_id = m.group(1)
+                entry_id = m.group(2)
+
+        combined_html = (launch_webpage or '') + '\n' + (submit_page or '') + '\n' + (redirect_page or '')
+
+        if not partner_id or not entry_id:
+            pid_match = re.search(r'(?:partner_?id|wid|p)["\'/=\s]+_?(\d{4,})', combined_html, re.IGNORECASE)
+            eid_match = re.search(r'(?:entry_?id)["\'/=\s]+([0-1]_[a-zA-Z0-9]{8})', combined_html, re.IGNORECASE)
+            if pid_match and eid_match:
+                partner_id = pid_match.group(1)
+                entry_id = eid_match.group(1)
+
+        if not partner_id or not entry_id:
+            raise ExtractorError('Unable to extract kaltura partner_id and entry_id')
+
+        ks = None
+        for r in [r'"ks"\s*:\s*"([^"]+)"', r'flashvars\[ks\]=([^&"\']+)', r'name=["\'](?:custom_)?ks["\']\s+value=["\']([^"\']+)["\']', r'(?:ks)["\'/=\s]+([a-zA-Z0-9_\-\|%]{20,})']:
+            ks_match = re.search(r, combined_html, re.IGNORECASE)
+            if ks_match:
+                ks = ks_match.group(1)
+                break
+
+        ks_param = f'/ks/{ks}' if ks else ''
+
+        direct_hls_url = f'https://cdnapisec.kaltura.com/p/{partner_id}/sp/{partner_id}00/playManifest/entryId/{entry_id}/format/applehttp/protocol/https{ks_param}'
 
         return {
             '_type': 'url',
-            'url': kultura_url,
-            'ie_key': 'Kaltura',
+            'url': direct_hls_url,
+            'id': entry_id,
+            'title': entry_id,
         }
